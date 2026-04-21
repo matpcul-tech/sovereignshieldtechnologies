d2451924e0315d32b8216d348eae1733d77f2e7e
matpcul-tech <matpcul@gmail.com>
2026-04-20 19:35:02 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SovereignShieldSite from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SovereignShieldSite />
+  </React.StrictMode>
+)
